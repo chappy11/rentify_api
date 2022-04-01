480b5b6b4ab72b7c10af54367acd2801fbf38fd3
Troy Sotto <70209287+ktsotto@users.noreply.github.com>
2022-04-02 02:05:06 +0800

    first commit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -23,324 +23,62 @@ class User extends Data_format{
 
     //register all user
     public function register_post(){
-        $data = $this->decode();
-           
-            $dat = array(
-                "email" => $data->email,
-                "password" => $data->password,
-                "user_pic" => "profiles/download.png",
-                "firstname" => $data->fname,
-                "lastname" => $data->lname,
-                "contact" => $data->contact,
-                "birthday" => "",
-                "street" => $data->sitio,
-                "barangay" => $data->brgy,
+         $user_pic = $_FILES['user_pic']['name'];
+       $license = $_FILES['license']['name'];
+        $fname = $this->post("fname");
+        $mname = $this->post("mname");
+        $lname = $this->post("lname");
+        $email = $this->post("email");
+        $password = $this->post("password");
+        $contact = $this->post("contact");
+
+       
+        if($this->User_Model->isEmailExist($email)){
+            $this->res(0,null,"Email is Already Exist",0);
+        }else{
+            $data = array(
+                "email" => $email,
+                "password" => $password,
+                "firstname" => $fname,
+                "middlename" => $mname,
+                "lastname" => $lname,
+                "contact" => $contact,
+                "user_pic" => "profiles/".$user_pic,
+                "license_pic" => "certification/".$license,
                 "user_type" => "user",
-                "user_status" => "active",
-                "service_status" => "none",
-                "service" => "none",
-                "isSubscribe" => 0
-            );
-            // $this->res(1,$dat,"Data",0);
-            $res = $this->User_Model->register($dat);
+                "isActive" => 1
+            ); 
+            $res = $this->User_Model->register($data);
             if($res){
-                $this->res(1,null,"Success",0);
+                move_uploaded_file($_FILES['user_pic']['tmp_name'],"profiles/".$user_pic);
+                move_uploaded_file($_FILES['license']['tmp_name'],"certification/".$license);
+                $this->res(1,null,"Successfully Registerd",0);
             }else{
-                $this->res(0,null,"Error network",0);
+                $this->res(0,null,"Something went wrong",0);
             }
+        }
+
     }
         
     
     
-    //login admin
-    public function loginAdmin_post(){
+
+    public function login_post(){
         $data = $this->decode();
         $email = isset($data->email) ? $data->email : "";
         $password = isset($data->password) ? $data->password : "";
-        if(empty($email) || empty($password)){
-            $this->res(0,null,"Fillout All Fields",0);
-        }else{
-            $res = $this->User_Model->login_admin($email,$password);
-            if(count($res) > 0){
-                $this->res(1,$res,"Successfully Login",1);
-            }else{
-                $this->res(0,null,"Wrong Credentials",0);
-            }
-        }
-    }
-
-
-    //login user
-    public function login_post(){
-        $d = $this->decode();
-       
-        $email = isset($d->email) ? $d->email : "";
-        $password = isset($d->password) ? $d->password : "";
         
-        if(empty($email) || empty($password)){
-            $this->res(1,null,"Fill out all fields",0);
-        }else{
-           $res = $this->User_Model->login($email,$password);
-           if(count($res)>0){
-                $this->res(1,$res,"Successfully login",0);
-           }else{
-                $this->res(0,null,"Wrong credentials",0);
-           }
-            
-        }
-    }
 
-    //get profile 
-    public function getprofile_get($id){
-        $data = $this->User_Model->getProfile($id);
-        if(count($data) > 0 ){
-            $this->res(1,$data,"Data found",count($data));
-        }else{
-            $this->res(0,null,"Data not found",0);
-        }
-    }
-
-    //update user details
-    public function update_post(){
-        $d = $this->decode();
-        $user_id = isset($d->user_id) ? $d->user_id : "";
-        $username = isset($d->username) ? $d->username : "";
-        $password = isset($d->password) ? $d->password : "";
-        
-        $data = array(
-            "username"=>$username,
-            "password"=>$password
-        );
-
-        $res = $this->User_Model->update($user_id,$data);
-        if($res){
-            $this->res(1,null,"Successfully updated");
-        }else{
-            $this->res(0,null,"Error updated");
-        }
-    }
-    
-    public function apply_post(){
-        $id = $this->post('id');
-        $type = $this->post("type");
-        $company = json_decode($this->post("company"));
-        $cert = $_FILES['cert']['name'];
-        $fac1 = $_FILES['fac1']['name'];
-        $fac2 = $_FILES['fac2']['name'];
-        $fac3 = $_FILES['fac3']['name'];
-
-        
-        $data = array(
-            "service" => $type,
-            "service_status" => "apply"
-        );
-
-        $update = $this->User_Model->update($id,$data);
-        if($update){
-            if(count($company) > 0){
-                foreach($company as $val){
-                    $comp = array(
-                        "user_id" => $id,
-                        "company_name" => $val->comp,
-                        "exp_years" => $val->yrs
-                    );
-                     $this->Company_Model->addComp($comp);
-                }
-            }
-        $cer = array(
-            "user_id" => $id,
-            "cert_pic" => "certification/".$cert
-        );
-        $this->Certification_Model->insert($cer);
-        $f1 = array(
-            "user_id" => $id,
-            "fac_pic" => "facility/".$fac1
-        );
-        $res = $this->Facility_Model->insert($f1);
-//        print_r($res);
-        $f2 = array(
-            "user_id" => $id,
-            "fac_pic" => "facility/".$fac2
-        );
-        $this->Facility_Model->insert($f2);
-        $f3 = array(
-            "user_id" => $id,
-            "fac_pic" => "facility/".$fac3
-        );
-        $this->Facility_Model->insert($f3);
-        move_uploaded_file($_FILES['cert']['tmp_name'],"certification/".$cert);
-        move_uploaded_file($_FILES['fac1']['tmp_name'],"facility/".$fac1);
-        move_uploaded_file($_FILES['fac2']['tmp_name'],"facility/".$fac2);
-        move_uploaded_file($_FILES['fac3']['tmp_name'],"facility/".$fac3);
-        $this->res(1,null,"Application Successfully Submitted",0);
-
-        }else{
-            $this->res(0,null,"Error Creating Application",0);
-        }
-    }
-    
-
-    public function updateInfo_post(){
-        $data = $this->decode();
-        $id = isset($data->id) ? $data->id : "";
-        $fname = isset($data->fname) ? $data->fname : "";
-        $lname = isset($data->lname) ? $data->lname : "";
-        $bday = isset($data->bday) ? $data->bday : "";
-        $contact = isset($data->contact) ? $data->contact : "";
-        $street = isset($data->street) ? $data->street : "";
-        $brgy = isset($data->brgy) ? $data->brgy : "";
-
-        if($this->isMobile($contact)){
-            $this->res(1,null,"Invalid Phone Number",0);
-        }else{
-            $arr = array(
-                "firstname" => $fname,
-                "lastname" => $lname,
-                "birthday" => $bday,
-                "contact" => $contact,
-                "street" => $street,
-                "barangay" => $brgy
-            );
-            $result = $this->User_Model->update($id,$arr);
-            if($result){
-                $this->res(1,null,"Succesfully Updated",0);
-            }else{
-                $this->res(0,null,"Error while updating",0);
-            }
-
-        } 
-    }
-
-    public function updatePass_post(){
-        $data = $this->decode();
-        $id = isset($data->id) ? $data->id : "";
-        $opass = isset($data->opass) ? $data->opass : "";
-        $npass = isset($data->npass) ? $data->npass : "";
-        $cpass = isset($data->cpass) ? $data->cpass : "";
-        
-        $result = $this->User_Model->getProfile($id);
-        if(empty($opass)||empty($npass) || empty($cpass)){
-            $this->res(0,null,"Fill out all fields",0);
-        }
-        else if($npass != $cpass){
-            $this->res(0,null,"Password does not match",0);
-        }
-        else if($opass != $result[0]->password){
-            $this->res(0,null,"Old Password Incorrect",0);
-        }else{
-            $arr = array(
-                "password" => $npass
-            );
-
-            $isUpdate = $this->User_Model->update($id,$arr);
-            if($isUpdate){
-                $this->res(1,null,"Successfully Updated",0);
-            }else{
-                $this->res(0,null,"Error Updated",0);
-            }
-        }
-        
-    }
-    
-    
-    public function sample_post(){
-        $file = $_FILES['videoFile']['name'];
-        $this->res(1,$file,"gg",0);
-        move_uploaded_file($_FILES['videoFile']['tmp_name'],"uploads/".$file);
-    }
-
-
-    public function sample2_post(){
-        
-        $data = json_decode($this->post('arr'));
-        $this->res(1,$data[0],"Data found",0);
-    }
-
-    public function getApplication_get(){
-       $result = $this->User_Model->getApplication();
-        if(count($result) > 0){
-            $this->res(1,$result,"Data found",0);
-        }
-    }
-
-    public function getServiceUser_get(){
-        $result = $this->User_Model->getServiceUser();
-         if(count($result) > 0){
-             $this->res(1,$result,"Data found",0);
-         }
-     }
-
-    public function validate_post(){
-        $data = $this->decode();
-        $id = $data->id;
-        $action = $data->actions;
-        $arr = array(
-            "service_status" => $action
-        );
-        $update = $this->User_Model->update($id,$arr);
-        if($update){
-            $this->res(1,null,"Successfully Updated",0);
-        }else{
-            $this->res(0,null,"Error Updated",0);
-        }
-
-    }
-
-    public function sample_get(){
-        $data= $this->Schedule_Model->lastIndex();
-        $this->res(1,$data,"Data found",0);
-    }
-  
-    public function changeStatus_post(){
-        $data = $this->decode();
-        $id = $data->id;
-        $action = $data->actions;
-
-        $arr = array(
-            "user_status" => $action
-        );
-
-        $result = $this->User_Model->update($id,$arr);
-        if($result){
-            $this->res(1,null,"Successfully Updated",0);
-        }else{
-            $this->res(0,null,"error",0);
-        }
-    }
-
-    public function acceptApplication_post(){
-       $data = $this->decode(); 
-       $id = $data->id;
-       $action = $data->actions;
-       $arr = array(
-           "service_status" => $action
-       );
-
-
-       $result = $this->User_Model->update($id,$arr);
-       if($result){
-           $this->res(1,null,"Successfully $action",0);
-       }else{
-           $this->res(0,null,"error",0);
-       }
-    }
-
-    public function changepic_post(){
-        $id = $this->post("id");
-        $image = $_FILES['pic']['name'];
-
-        $arr = array(
-            "user_pic" => "profiles/".$image
-        );
-
-        $result = $this->User_Model->update($id,$arr);
-        if($result){
-            move_uploaded_file($_FILES['pic']['tmp_name'],"profiles/".$image);
-            $this->res(1,null,"Successfully Updated",0);
+        $resp = $this->User_Model->login($email,$password);
+        if(count($resp) > 0) {
+            $this->res(1,null,"Succesfully Login",0);
         }else{
             $this->res(0,null,"Error",0);
         }
+        
     }
+
+
 }
 
 
