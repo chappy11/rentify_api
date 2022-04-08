a56c1f8fec52db7d5ffa73495f2c0dccf8b4eb35
Jeffrey <jeffrey.mata@mlhuillier.com>
2022-04-08 16:06:59 +0800

    Motourista

--- application/controllers/Motourista.php
+++ application/controllers/Motourista.php
@@ -11,10 +11,33 @@ include_once(dirname(__FILE__)."/Data_format.php");
         public function becomeMotourista_post(){
             $data = $this->decode();
             $user_id = isset($data->$user_id) ? $data->user_id : "";   
+            $name = isset($data->name) ? $data->name : "";
+            $lat = isset($data->lat) ? $data->lat : "";
+            $lng = isset($data->lng) ? $data->lng : "";
             
-        }
-    
-        
+            $payload = array(
+                "user_id" => $user_id,
+                "motour_name" => $name,
+                "latitude" => $lat,
+                "longitude" => $lng  
+                "isActive" => 0
+            );
 
+            $resp = $this->Motourista_Model->addMotourista($payload);
+            if($resp){
+                $this->res(1,null,"Successfully Added",0);
+            }else{
+                $this->res(0,null,"Something went wrong",0);
+            }
+        }
+        
+        public function getmotouristabyuser_get($user_id){
+            $data = $this->Motourista_Model->getmotouristabyuser($user_id);
+            if(count($data) > 0){
+                $this->res(1,$data,"Data found",count($data));
+            }else{
+                $this->res(0,null,"No data found",0);
+            }
+        }
     }
 ?>
