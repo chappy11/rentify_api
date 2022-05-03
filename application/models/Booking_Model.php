595524e68c3e0628eeac4363e8f621cf3c748dca
chappy11 <76852841+chappy11@users.noreply.github.com>
2022-05-03 15:23:11 +0800

    Add files via upload

--- application/models/Booking_Model.php
+++ application/models/Booking_Model.php
@@ -16,9 +16,18 @@ class Booking_Model extends CI_Model{
     public function checkpending($user_id,$motor_id){
         $this->db->select("*");
         $this->db->from($this->table);
-        $this->db->where("motor_id",$motor_id);
         $this->db->where("user_id",$user_id);
         $this->db->where("booking_status",0);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getbyuserid($user_id){
+        $this->db->select("*");
+        $this->db->from($this->table);
+        $this->db->where("booking.user_id",$user_id);
+        $this->db->where("booking.booking_status<",4);
+        $this->db->join("vehicle","vehicle.motor_id=booking.motor_id");
         $query = $this->db->get();
         return $query->result();
     }
@@ -74,7 +83,7 @@ class Booking_Model extends CI_Model{
         $this->db->select("start_date,end_date");
         $this->db->from($this->table);
         $this->db->where("motor_id",$motor_id);
-        $this->db->where("booking_status",0);
+        $this->db->where("booking_status",1);
         $query = $this->db->get();
         return $query->result();
     }
