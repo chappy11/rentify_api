5909e5031ac9d924244429f500f1f5154be16ebc
chappy <lumangyao112@gmail.com>
2023-06-25 23:51:41 +0800

    fix

--- application/controllers/Review.php
+++ application/controllers/Review.php
@@ -5,19 +5,39 @@
 
         public function __construct(){
             parent::__construct();
-            $this->load->model(array("Review_Model"));
+            $this->load->model(array("Review_Model",'UserOrder_Model','OrderItem_Model'));
         }
 
         public function create_post(){
             $data = $this->decode();
-            $resp = $this->Review_Model->create($data);
+            $user_id = $data->user_id;
+            $product_id = $data->product_id;
 
-            if($resp){
-                $this->res(1,null,"Successsfully Created",0);
-            }else{
-                $this->res(0,null,"Something went wrong",0);
+            $orders = $this->UserOrder_Model->getOrderByUserId($user_id);
+            $hasError = 0;
+            foreach($orders as $val){
+                $orderItem = $this->OrderItem_Model->getOrderItemByOrderId($val->order_id);
+                foreach($orderItem as $item){
+                    if($item->product_id == $product_id){
+                        $hasError = 1;
+                    }
+                }
             }
-        }
+
+            if($hasError == 0){
+                $this->res(0,null," Order first before reviewing this product",0);
+            }else{
+                $resp = $this->Review_Model->create($data);
+
+                if($resp){
+                    $this->res(1,null,"Successsfully Created",0);
+                }else{
+                    $this->res(0,null,"Something went wrong",0);
+                }
+    
+            }            
+
+       }
         
         public function reviews_get($product_id){
             $data = $this->Review_Model->getReview($product_id);
